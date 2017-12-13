b05666b3b30042bfc21ed47a8012110a6fdbadb0
foscocanc <foscocanc@gmail.com>
2017-12-13 14:53:51 +0100

    Problem in finding windows

--- polar_analyser.py
+++ polar_analyser.py
@@ -1,9 +1,24 @@
 import numpy as np
 import csv
+import os
 
 def findwindow (data,start,sizewindow = 300):
-    index = np.where(data[:,0]>data[start][0]+sizewindow)[0][0]
-    return index
+    print(data[start].shape)
+    print (start)
+    index = np.argwhere(data[:,0]>data[start][0]+sizewindow)
+    # print(index)
+    if index.any():
+        print('ok')
+        return index[0]
+    else:
+        print('no')
+    # if index[0]==[]:
+    #     return index [0]
+    # else:
+    #     print (index[0][0])
+    #     return index[0][0] 
+        
+    
 
 def getRR(data):
     RR = []
@@ -27,17 +42,15 @@ def statistic(data,startwindow,endwindow):
     pnn100 = pNN100(data[:,1])
     return m,sd,pnn100
 
-
-
-def ba_analysis():#devName,address):
+def testfunction():#devName,address):
     #Open csv file
     # filename  =  devName.split(' ')[2]+'.csv'
     filename = 'test.csv'
     try:
         with open(filename, newline = '\n') as csvfile:
-            data = csv.reader(csvfile, delimiter = '\t',quotechar = '|')
+            csvfile.readline() #skip the headers
+            data = csv.reader(csvfile, delimiter = '\t',quotechar = '|',quoting=csv.QUOTE_NONNUMERIC)
             data = list(data)
-            data.pop(0)
             data = np.array(data)
         pass
     except Exception as e:
@@ -45,19 +58,83 @@ def ba_analysis():#devName,address):
     #find last window and flush the rest of the data
     lastid = data[-1][2]
     data = data[np.where(data[:,2] == lastid)]
-    
-   #interpolazione se necesssaria
-    for i in range(len(data)):
+
+    #interpolazione se necesssaria
+    for i in range(len(data)-1):
         delta_t = data[i+1][0]-data[i][0]
         if (delta_t>1.5):
-            n_dati = int(delta_t)-1
+            n_dati = int(np.floor(delta_t))+1
             intpolTime = np.linspace(data[i][0],data[i+1][0],n_dati)
             intpolBPM = np.linspace(data[i][1],data[i+1][1],n_dati)
-            intpolID = [lastid for x in range(n_dati-1)]
+            intpolID = np.array([lastid for x in range(n_dati)])
+            ins=np.stack((intpolTime,intpolBPM,intpolID), axis=1)
+            # ins = np.array([intpolTime,intpolBPM,intpolID])
+            # ins = np.transpose(ins)
+            print (ins)
+            for k in range(1,len(ins)-1):
+                data = np.insert(data,[i+k],ins[k],axis=0)
+    # Cerca finestre
+    with open('data_out.csv','w') as out:
+        for i in data:
+            for k in i:
+                out.write(str(k)+'\t')
+            out.write('\n')
+
+
+    # filename  =  devName.split(' ')[2]+'Analysis.csv'
+    filename = 'test_out.csv'
+    if(os.path.isfile('./'+filename)):
+        filePointer  =  open(filename, 'a')
+    else:
+        filePointer  =  open(filename, 'w')
+        filePointer.write('TIME_START\tTIME_END\tMEAN\tSDNN\tpNN100\n')
+
+    startwindow = 0
+    RR = getRR(data)
+    while True:
+        endwindow = findwindow(data,startwindow)# finestra da 5 min
+        if endwindow:
+            m,sd,pnn100 = statistic(RR,startwindow,endwindow-1)
+            output  =  str(RR[startwindow][0]) + '\t' + str(RR[endwindow-1][0]) +'\t' + str(m) +'\t' + str(sd) +'\t' + str(pnn100) + '\n'
+            filePointer.write(output) 
+            startwindow = findwindow(data,startwindow,20)
+            if not startwindow:
+                break
+        else:
+            break
+    else:
+        filePointer.close()
+
+def ba_analysis():#devName,address):
+    #Open csv file
+    # filename  =  devName.split(' ')[2]+'.csv'
+    filename = 'test.csv'
+    try:
+        with open(filename, newline = '\n') as csvfile:
+            csvfile.readline() #skip the headers
+            data = csv.reader(csvfile, delimiter = '\t',quotechar = '|',quoting=csv.QUOTE_NONNUMERIC)
+            data = list(data)
+            data = np.array(data)
+        pass
+    except Exception as e:
+        print (e)
+    #find last window and flush the rest of the data
+    lastid = data[-1][2]
+    data = data[np.where(data[:,2] == lastid)]
+
+    #interpolazione se necesssaria
+    for i in range(len(data)-1):
+        delta_t = data[i+1][0]-data[i][0]
+        if (delta_t>1.5):
+            n_dati = int(delta_t)+1
+            intpolTime = np.linspace(data[i][0],data[i+1][0],n_dati)
+            intpolBPM = np.linspace(data[i][1],data[i+1][1],n_dati)
+            intpolID = [lastid for x in range(n_dati)]
             ins = np.array([intpolTime,intpolBPM,intpolID])
             ins = np.array(ins)
-            for k in range(len(ins)):
-                data = np.insert(data,[i+k],ins[k])
+            ins = np.transpose(ins)
+            for k in range(1,len(ins)-1):
+                data = np.insert(data,[i+k],ins[k],axis=0)
             
     # Cerca finestre
 
@@ -79,7 +156,7 @@ def ba_analysis():#devName,address):
         output  =  str(RR[startwindow][0]) + '\t' + str(RR[endwindow-1][0]) +'\t' + str(m) +'\t' + str(sd) +'\t' + str(pnn100) + '\n'
         filePointer.write(output) 
         startwindow = findwindow(data,startwindow,20)
-        if startwindow is None or startwindow =  = 0:
+        if startwindow is None or startwindow == 0:
             break
     else:
         filePointer.close()
