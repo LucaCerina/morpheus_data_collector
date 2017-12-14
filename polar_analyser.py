2da9265104b8a17b78a34a3a4056417783d80c43
foscocanc <foscocanc@gmail.com>
2017-12-14 11:47:24 +0100

    created smaller subroutines and a draft for live analysis

--- polar_analyser.py
+++ polar_analyser.py
@@ -1,8 +1,36 @@
 import os
-import csv
 import numpy as np
+from time import sleep
 
-def findwindow (data, start, sizewindow = 300):
+def load():#devName,address):
+    #Open csv file
+    # filename  =  devName.split(' ')[2]+'.csv'
+    filename = 'test.csv'
+    try:
+        data = np.genfromtxt(filename, dtype=float, skip_header=1)
+        #find last window and flush the rest of the data
+        lastid = data[-1][2]
+        data = data[np.where(data[:, 2] == lastid)]
+        return data, lastid
+    except Exception as e:
+        print(e)
+
+def interpolation(data,WID):
+    i=0
+    while i<len(data)-2:
+        delta_t = data[i+1][0]-data[i][0]
+        if (delta_t > 1.5):
+            n_dati = int(np.floor(delta_t))+1
+            intpolTime = np.linspace(data[i][0], data[i+1][0], n_dati)
+            intpolBPM = np.linspace(data[i][1], data[i+1][1], n_dati)
+            intpolID = np.array([WID for x in range(n_dati)])
+            ins = np.stack((intpolTime, intpolBPM, intpolID), axis=1)
+            for k in range(1,len(ins)-1):
+                data = np.insert(data, [i+k], ins[k], axis=0)
+        i+=(1+len(ins))
+    return data
+
+def findwindow (data, start=0, sizewindow = 300):
     index = np.argwhere(data[:, 0]>data[start][0]+sizewindow)
     if index.any():
         return index[0,0]
@@ -12,7 +40,7 @@ def getRR(data):
     for i in range(len(data)):
         RR.append([data[i][0], 1/data[i][1]])
     RR = np.array(RR)
-    print(RR)
+    print(RR)lastid
     return RR
 
 def pNN100(RR):
@@ -30,9 +58,50 @@ def statistic(data, startwindow, endwindow):
     pnn100 = pNN100(data[startwindow:endwindow][1])
     return m, sd, pnn100
 
+def parse_write(data,startwindow,filePointer):
+    # Cerca finestre
+        RR = getRR(data)
+        while True:
+            endwindow = findwindow(data,startwindow)# finestra da 5 min data
+            if endwindow:
+                m,sd,pnn100 = statistic(RR,startwindow,endwindow)
+                output  =  str(RR[startwindow][0]) + '\t' + str(RR[endwindow][0]) +'\t' + str(m) +'\t' + str(sd) +'\t' + str(pnn100) + '\n'
+                filePointer.write(output)
+                startwindow = findwindow(data,startwindow,20)
+                if not startwindow:
+                    return endwindow+1 #Ritorna l'indice della finestra da analizzare
+            else:
+                return startwindow
 
 
-def ba_analysis():#devName,address):
+def therad_analysis():#devName,address):
+    #Open csv file
+    # filename  =  devName.split(' ')[2]+'.csv'
+    sleep(300) #Wait at least 5 min for a 
+    failures=0
+    # data, lastid = load()
+    if(os.path.isfile('./'+filename)):
+        filePointer  =  open(filename, 'a')
+    else:
+        filePointer  =  open(filename, 'w')
+        filePointer.write('TIME_START\tTIME_END\tMEAN\tSDNN\tpNN100\n')
+    
+    startwindow=0
+
+    while(failures<3):
+        data, lastid = load()
+        if findwindow(data):
+            data=interpolation(data,lastid)
+            startwindow=parse_write(data,startwindow,filePointer)
+        else:
+            #Idle per 20 s , fail counter + 1 e cicla
+            sleep(20)
+            failures+=1
+    else:
+        filePointer.close()
+
+
+def batch_analysis():#devName,address):
     #Open csv file
     # filename  =  devName.split(' ')[2]+'.csv'
     filename = 'test.csv'
@@ -64,29 +133,32 @@ def ba_analysis():#devName,address):
                 data = np.insert(data, [i+k], ins[k], axis=0)
         i+=(1+len(ins))
 
+    # Controllo se la finestra di lettura ha durata abbastanza lunga da avere significanza
+    if findwindow(data,0):
+        #Apro il file
 
-    # Cerca finestre
-    # filename  =  devName.split(' ')[2]+'Analysis.csv'
-    filename = 'test_out.csv'
-    if(os.path.isfile('./'+filename)):
-        filePointer  =  open(filename, 'a')
-    else:
-        filePointer  =  open(filename, 'w')
-        filePointer.write('TIME_START\tTIME_END\tMEAN\tSDNN\tpNN100\n')
+        # filename  =  devName.split(' ')[2]+'Analysis.csv'
+        filename = 'test_out.csv'
 
-    startwindow = 0
-    RR = getRR(data)
-    while True:
-        endwindow = findwindow(data,startwindow)# finestra da 5 min
-        if endwindow:
-            m,sd,pnn100 = statistic(RR,startwindow,endwindow)
-            output  =  str(RR[startwindow][0]) + '\t' + str(RR[endwindow][0]) +'\t' + str(m) +'\t' + str(sd) +'\t' + str(pnn100) + '\n'
-            filePointer.write(output)
-            startwindow = findwindow(data,startwindow,20)
-            if not startwindow:
-                break
-            startwindow=startwindow
+        if(os.path.isfile('./'+filename)):
+            filePointer  =  open(filename, 'a')
         else:
-            break
-    else:
-        filePointer.close()
+            filePointer  =  open(filename, 'w')
+            filePointer.write('TIME_START\tTIME_END\tMEAN\tSDNN\tpNN100\n')
+
+        # Cerca finestre
+        RR = getRR(data)
+        startwindow = 0
+        while True:
+            endwindow = findwindow(data,startwindow)# finestra da 5 min data
+            if endwindow:
+                m,sd,pnn100 = statistic(RR,startwindow,endwindow)
+                output  =  str(RR[startwindow][0]) + '\t' + str(RR[endwindow][0]) +'\t' + str(m) +'\t' + str(sd) +'\t' + str(pnn100) + '\n'
+                filePointer.write(output)
+                startwindow = findwindow(data,startwindow,20)
+                if not startwindow:
+                    break
+            else:
+                break
+        else:
+            filePointer.close()
