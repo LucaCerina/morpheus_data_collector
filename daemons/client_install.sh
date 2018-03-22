c82b669b63d4c874adddc38169b1edc9be7d0c31
LcCerina <luca.cerina@polimi.it>
2018-03-22 14:48:33 +0100

    added client scanner daemon

---
+++ daemons/client_install.sh
@@ -0,0 +1,20 @@
+SYSTEMD_PATH=/lib/systemd/system
+
+
+echo "[UNIT]
+Descriptions= <Polar Scanner>
+ConditionPathExists=$PWD/../client-server-mode
+After=multi-user.target
+
+[Service]
+Type=idle
+User=root
+Group=root
+ExecStart=/usr/bin/python3.5 $PWD/../client-server-mode/polar_scanner_net.py
+Restart=always
+
+[Install]
+WantedBy=multi-user.target" > $SYSTEMD_PATH/polar_scan.service
+
+systemctl start polar_scan.service
+
