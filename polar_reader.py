4faa5537ada696d2acaff3a2ffa0276daa41cdfe
LcCerina <luca.cerina@polimi.it>
2018-05-02 16:34:44 +0200

    fixed file issues

--- polar_reader.py
+++ polar_reader.py
@@ -32,24 +32,29 @@ def heartRateThread(devName, address):
         deviceName = devName.split(' ')[2]
         filename = deviceName +'.csv'
         if(os.path.isfile('./'+filename)):
-            filePointer = open(filename, 'a+')
-            filePointer.seek(filePointer.tell() - 20) #TODO: improve the hardcoded value
-            lastLine = filePointer.read()
-            try:
-                readIdx = int(lastLine.split("\t")[-1].split("\n")[0]) + 1
-            except ValueError:
-                # Catch exception when only headers are available on the file
+            filePointer = open(filename, 'a+', 0)
+            filePosition = filePointer.tell() - 20
+            if filePosition > 0:
+                filePointer.seek(filePosition #TODO: improve the hardcoded value
+                lastLine = filePointer.read()
+                try:
+                    readIdx = int(lastLine.split("\t")[-1].split("\n")[0]) + 1
+                except ValueError:
+                    # Catch exception when only headers are available on the file
+                    readIdx = 0
+            else:
+                filePointer.write('TIME\tHR\tRR\tWID\n')
                 readIdx = 0
         else:
-            filePointer = open(filename, 'w')
+            filePointer = open(filename, 'w', 0)
             filePointer.write('TIME\tHR\tRR\tWID\n')
 
-        monitor.startMonitor()
+        monitorStarted = monitor.startMonitor()
 
         # Initialize sampleTime
         sampleTimeOld = time() 
         # Reader continuous loop
-        while(True):
+        while(True and monitorStarted):
             try:
                 reading = monitor.getHeartRate()
                 sampleTimeNew = time()
@@ -87,7 +92,7 @@ def heartRateThread(devName, address):
                     # monitor.terminate()
                     #monitor = HRmonitor(devName, address)
                     if(monitor.device != None):
-                        monitor.startMonitor()
+                        monitorStarted = monitor.startMonitor()
                     else:
                         sleep(0.1)
                 else:
