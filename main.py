29a5557b6a6e51602120ac051485d4e829f8ac1b
LcCerina <luca.cerina@polimi.it>
2019-05-09 19:31:18 +0100

    integrated csv writing, added new sgp30

--- main.py
+++ main.py
@@ -20,15 +20,23 @@ import digitalio
 import board
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
-from sgp30 import Sgp30
+#from sgp30 import Sgp30
+from adafruit_sgp30 import Adafruit_SGP30 as sgp30
 from smbus2 import SMBusWrapper
-
+import csv
 
 # thread controlling temperature and humidity
 def TH_thread(config):
     thsensor = si7021.si7021(1)
-    # List to save data when connection is absent
+    
+    # Memory backlog
     backlog_record = []
+    # File backlog
+    file = open("TH.csv","a+", newline='', encoding = "utf-8")
+    writer = csv.writer(file, dialect='excel', lineterminator='\n')
+    if(file.tell() == 0):
+        writer.writerow(["TIME","TEMPERATURE","HUMIDITY"])
+    #file.close()
     # header for API requests
     headers = {'Content-Type':'application/json'}
     headers['token'] = config['token']
@@ -45,6 +53,16 @@ def TH_thread(config):
         data = {}
         data['token'] = config['token']
         data['input'] = {'room_id': config['room_id'], 'timestamp': timestamp, 'temperature': json.dumps(temperature), 'humidity': json.dumps(humidity)} 
+        
+        # Il file csv per ora lo lasciamo fuori quindi viene fatto indipendentemente dalla connessione o meno
+        #file = open("TH.csv","a", encoding = "utf-8")
+        #writer = csv.writer(file)
+        writer.writerow([timestamp,temperature,humidity])
+        file.flush()
+        #file.close()
+        #file = open("TH.csv","r+", encoding = "utf-8")
+        #file.read()
+
         # Send data to server
         try:
             print("Sending T {0:2.5} H {1:2.5} at time {2:}".format(temperature, humidity, timestamp))
@@ -60,11 +78,21 @@ def TH_thread(config):
         except requests.exceptions.ConnectionError:
             print("Connection refused: saving TH record on backlog")
             backlog_record.append(data)
+        except KeyboardInterrupt:
+            file.close()
+            break
 
 # Thread controlling CO2 readings
 def carbon_thread(config):
-    # List to save data when connection is absent
+    # Memory backlog
     backlog_record = []
+    # File backlog
+    file = open("CARB.csv","a+", newline='', encoding = "utf-8")
+    writer = csv.writer(file, dialect='excel', lineterminator='\n')
+    if(file.tell() == 0):
+        writer.writerow(["TIME","CO2"])
+    #file.close()
+
     # header for API requests
     headers = {'Content-Type':'application/json'}
     headers['token'] = config['token']
@@ -73,55 +101,89 @@ def carbon_thread(config):
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(20, GPIO.IN)
     
-    with SMBusWrapper(1) as bus:
-        sgp = Sgp30(bus, baseline_filename = "sgpbaseline.txt")
-        if(GPIO.input(20) == 1):
-            print("Init baseline and store it")
-            sgp.init_sgp()
-            sgp.store_baseline()
-        else:
-            print("get baseline from file")
-            sgp.try_set_baseline()
-            sgp.store_baseline()
-        
-        sgp.read_measurements()
-        # Warm up the sensor
+    #with SMBusWrapper(1) as bus:
+
+    i2c_bus = busio.I2C(board.SCL, board.SDA, frequency=100000)
+    sgp = sgp30(i2c_bus) #Sgp30(bus, baseline_filename = "sgpbaseline.txt")
+    if(GPIO.input(20) == 1):
+        print("Init baseline and store it")
+        sgp.iaq_init()
+        # Warm up IAQ
         for _ in range(20):
             sleep(1)
-            sgp.read_measurements()
+            sgp.iaq_measure()
+        baseline = sgp.get_iaq_baseline()
+        print("baseline {}".format(baseline))
+        with open("sgpbaseline.txt", "w") as base:
+            json.dump(baseline, base)
+            base.close()
+    else:
+        with open("sgpbaseline.txt", "r") as base:
+            baseline = json.load(base)
+            base.close()
+        print("get baseline from file")
+        sgp.set_iaq_baseline(baseline[0], baseline[1])
+    
+    #sgp.read_measurements()
+    sgp.iaq_measure()
+    # Warm up the sensor
+    for _ in range(20):
+        sleep(1)
+        #sgp.read_measurements()
+        sgp.iaq_measure()
 
-        while(True):
-            # Wait 1 second to keep sensor active
-            for _ in range(30):
-                sleep(1)
-                # Read data from sensors
-                co2 = sgp.read_measurements()
-                co2 = getattr(co2, "data")[0]
-            # Assemble data
-            timestamp = udatetime.to_string(udatetime.now())
-            data = {}
-            data['token'] = config['token']
-            data['input'] = {'room_id': config['room_id'], 'timestamp': timestamp, 'co2': json.dumps(co2)} 
-            # Send data to server
-            try:
-                print("Sending CO2 {0:2} at time {1:}".format(co2, timestamp))
-                req = requests.post(url=URL, headers=headers, json=data)
+    while(True):
+        # Wait 1 second to keep sensor active
+        for _ in range(30):
+            sleep(1)
+            # Read data from sensors
+            co2, _ = sgp.iaq_measure() #read_measurements()
+            #co2 = getattr(co2, "data")[0]
+        # Assemble data
+        timestamp = udatetime.to_string(udatetime.now())
+        
+        #File csv
+        #file = open("CO2.csv","a", encoding = "utf-8")
+        #writer = csv.writer(file)
+        writer.writerow([timestamp,co2])
+        file.flush()
+        #file.close()
+        #file = open("CO2.csv","r+", encoding = "utf-8")
+        #file.read()
 
-                # Check request result
-                if req.status_code != 200:
-                    print("Req status {}:saving CO2 record on backlog".format(req.status_code))
-                    backlog_record.append(data)
-                else:
-                    while len(backlog_record) > 0 and requests.post(url=URL, headers=headers, json=backlog_record[0]).status_code == 200:
-                        del backlog_record[0]
-            except requests.exceptions.ConnectionError:
-                print("Connection refused: saving CO2 record on backlog")
+        data = {}
+        data['token'] = config['token']
+        data['input'] = {'room_id': config['room_id'], 'timestamp': timestamp, 'co2': json.dumps(co2)} 
+        # Send data to server
+        try:
+            print("Sending CO2 {0:} at time {1:}".format(co2, timestamp))
+            req = requests.post(url=URL, headers=headers, json=data)
+
+            # Check request result
+            if req.status_code != 200:
+                print("Req status {}:saving CO2 record on backlog".format(req.status_code))
                 backlog_record.append(data)
+            else:
+                while len(backlog_record) > 0 and requests.post(url=URL, headers=headers, json=backlog_record[0]).status_code == 200:
+                    del backlog_record[0]
+        except requests.exceptions.ConnectionError:
+            print("Connection refused: saving CO2 record on backlog")
+            backlog_record.append(data)
+        except KeyboardInterrupt:
+            file.close()
+            break
 
 # Thread to read from TSL sensor ATTENTION SENSOR NEED ADDITONAL FRONTEND TO READ DATA CORRECTLY
 def light_thread(config):
-    # List to save data when connection is absent
+    # Memory backlog
     backlog_record = []
+    # File backlog
+    file = open("LHT.csv","a+", newline='', encoding = "utf-8")
+    writer = csv.writer(file, dialect='excel', lineterminator='\n')
+    if(file.tell() == 0):
+        writer.writerow(["TIME","LIGHT"])
+    #file.close()
+
     # header for API requests
     headers = {'Content-Type':'application/json'}
     headers['token'] = config['token']
@@ -147,6 +209,14 @@ def light_thread(config):
 
         # Assemble data
         timestamp = udatetime.to_string(udatetime.now())
+        # Csv file
+        #file = open("light.csv","a", encoding = "utf-8")
+        #writer = csv.writer(file)
+        writer.writerow([timestamp,light])
+        file.flush()
+        #file.close()
+        #file = open("light.csv","r+", encoding = "utf-8")
+        #file.read()
         data = {}
         data['token'] = config['token']
         data['input'] = {'room_id': config['room_id'], 'timestamp': timestamp, 'light': json.dumps(light)} 
@@ -165,11 +235,21 @@ def light_thread(config):
         except requests.exceptions.ConnectionError:
             print("Connection refused: saving LIGHT record on backlog")
             backlog_record.append(data)
+        except KeyboardInterrupt:
+            file.close()
+            break
 
 # Thread TEMPORARY to read noise from ADC. it sends energy on 30 seconds
 def noise_thread(config):
-        # List to save data when connection is absent
+    # Memory backlog
     backlog_record = []
+    # File backlog
+    file = open("NOI.csv","a+", newline='', encoding = "utf-8")
+    writer = csv.writer(file, dialect='excel', lineterminator='\n')
+    if(file.tell() == 0):
+        writer.writerow(["TIME","NOISE"])
+    #file.close()
+
     # header for API requests
     headers = {'Content-Type':'application/json'}
     headers['token'] = config['token']
@@ -202,6 +282,15 @@ def noise_thread(config):
 
         # Assemble data
         timestamp = udatetime.to_string(udatetime.now())
+        # saving data inside the csv file
+        #file = open("noise.csv","a", encoding = "utf-8")
+        #writer = csv.writer(file)
+        writer.writerow([timestamp,noise])
+        file.flush()
+        #file.close()
+        #file = open("noise.csv","r+", encoding = "utf-8")
+        #file.read() 
+        
         data = {}
         data['input'] = {'room_id': config['room_id'], 'timestamp': timestamp, 'noise': json.dumps(noise)} 
         data['token'] = config['token']
@@ -220,6 +309,9 @@ def noise_thread(config):
         except requests.exceptions.ConnectionError:
             print("Connection refused: saving NOISE record on backlog")
             backlog_record.append(data)
+        except KeyboardInterrupt:
+            file.close()
+            break
 
 def userLogin(username, password):
     headers = {'Content-Type': 'application/json'}
@@ -254,12 +346,12 @@ if __name__ == "__main__":
     t5 = threading.Thread(target = scanner.start, args=(config,), name = "POLAR") 
 
     t1.start()
-    sleep(0.2)
+    sleep(0.5)
     t2.start()
-    sleep(0.2)
+    sleep(0.5)
     t3.start()
-    sleep(0.2)
+    sleep(0.5)
     t4.start()
-    sleep(0.2)
-    t5.start()
+    sleep(0.5)
+    #t5.start()
 
